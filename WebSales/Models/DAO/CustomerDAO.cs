1d682b22c41dc50315085be446d05684dc3128d3
NGO CONG MINH <1621050237@student.humg.edu.vn>
2021-10-30 15:29:32 +0700

    Edit UI Products

---
+++ WebSales/Models/DAO/CustomerDAO.cs
@@ -0,0 +1,105 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebSales.Models.EF;
+
+namespace WebSales.Models.DAO
+{
+    public class CustomerDAO : BaseDAO, ICustomerDAO
+    {
+        public async Task<bool> Add(Customer entity)
+        {
+            try
+            {
+                _context.Customers.Add(entity);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            try
+            {
+                Customer entity = await GetSingleByID(id);
+
+                _context.Customers.Remove(entity);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public async Task<List<Customer>> GetAll()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+
+            return customers;
+        }
+
+        public async Task<List<Customer>> GetByKeyword(string keyword)
+        {
+            List<Customer> customers = await _context.Customers
+                 .Where(t => t.ID.Contains(keyword))
+                 .ToListAsync();
+
+            return customers;
+        }
+
+        public async Task<IPagedList<Customer>> GetByPaged(int page, int pageSize, string keyword)
+        {
+            List<Customer> customers = await GetByKeyword(keyword);
+
+            IPagedList<Customer> pagedListCustomers = customers.ToPagedList(page, pageSize);
+
+            return pagedListCustomers;
+        }
+
+        public async Task<Customer> GetSingleByID(string id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            return customer;
+        }
+
+        public async Task<bool> Update(Customer entity)
+        {
+            try
+            {
+                Customer cEntity = await GetSingleByID(entity.ID);
+
+                if (cEntity != null)
+                {
+                    cEntity.Password = entity.Password;
+                    cEntity.FullName = entity.FullName;
+                    cEntity.EMail = entity.EMail;
+                    cEntity.Photo = entity.Photo;
+                    cEntity.Activated = entity.Activated;
+
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ WebSales/Models/DAO/OrderDetailDAO.cs
@@ -0,0 +1,131 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebSales.Models.EF;
+
+namespace WebSales.Models.DAO
+{
+    public class OrderDetailDAO : BaseDAO, IOrderDetailDAO
+    {
+        public async Task<bool> Add(OrderDetail entity)
+        {
+            try
+            {
+                _context.OrderDetails.Add(entity);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            try
+            {
+                OrderDetail entity = await GetSingleByID(id);
+
+                _context.OrderDetails.Remove(entity);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> DeleteByOrderID(int orderID)
+        {
+            try
+            {
+                List<OrderDetail> orderDetails = await GetByOrderID(orderID);
+
+                _context.OrderDetails.RemoveRange(orderDetails);
+
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return false;
+        }
+
+        public async Task<List<OrderDetail>> GetAll()
+        {
+            List<OrderDetail> orderDetails = await _context.OrderDetails.ToListAsync();
+
+            return orderDetails;
+        }
+
+        public async Task<List<OrderDetail>> GetByKeyword(string keyword)
+        {
+            List<OrderDetail> orderDetails = await _context.OrderDetails
+                 .Where(t => t.Product.Name.Contains(keyword))
+                 .ToListAsync();
+
+            return orderDetails;
+        }
+
+        public async Task<List<OrderDetail>> GetByOrderID(int orderID)
+        {
+            List<OrderDetail> orderDetails = await _context.OrderDetails
+                .Where(t => t.OrderID == orderID)
+                .ToListAsync();
+
+            return orderDetails;
+        }
+
+        public async Task<IPagedList<OrderDetail>> GetByPaged(int page, int pageSize, string keyword)
+        {
+            List<OrderDetail> orderDetails = await GetByKeyword(keyword);
+
+            IPagedList<OrderDetail> pagedListOrderDetails = orderDetails.ToPagedList(page, pageSize);
+
+            return pagedListOrderDetails;
+        }
+
+        public async Task<OrderDetail> GetSingleByID(int id)
+        {
+            OrderDetail orderDetail = await _context.OrderDetails.FindAsync(id);
+
+            return orderDetail;
+        }
+
+        public async Task<bool> Update(OrderDetail entity)
+        {
+            try
+            {
+                OrderDetail cEntity = await GetSingleByID(entity.ID);
+
+                if (cEntity != null)
+                {
+                    cEntity.UnitPrice = entity.UnitPrice;
+                    cEntity.Quantily = entity.Quantily;
+
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return false;
+        }
+    }
+}
